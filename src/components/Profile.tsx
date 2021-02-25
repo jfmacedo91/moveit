375cd8a6dcf0922abf73077a3c95cd771c9f09b7
Jean Fernandes de Macedo <jfmacedo91@gmail.com>
2021-02-24 23:21:58 -0300

    class three completed

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,12 @@
+import { useContext } from 'react';
+
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={ styles.profileContainer }>
       <img src="https://github.com/JFMacedo.png" alt="Jean Fernandes de Macedo" />
@@ -8,7 +14,7 @@ export function Profile() {
         <strong>Jean Macedo</strong>
         <p>
           <img src="icons/level.svg" alt="levelUp" />
-          Level 1
+          Level { level }
         </p>
       </div>
     </div>
